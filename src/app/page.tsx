089cacc80e4ad576bc158b11f38bd7a8d9143cdb
Ucode-uvais <seyyeduvais@gmail.com>
2025-10-09 14:17:41 +0530

    Features Implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Hero from "@/modules/hero/Hero";
+import Features from "@/modules/features/Features";
+import Pricing from "@/modules/pricing/Pricing";
 
 const page = () => {
   return (
     <div>
       <Hero />
+      <Features />
+      <Pricing />
     </div>
   );
 };
